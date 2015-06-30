5afdd75adfe072b2817f3a6b8db08a6723807494
Bala Paranj <bparanj@gmail.com>
2015-06-30 14:13:42 -0700

    updated tests to use expect syntax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
-  # attr_accessible :name, :price, :url
 
   validates :name, :presence => true
   validates :price, :presence => true
--- spec/helpers/aws_helper_spec.rb
+++ spec/helpers/aws_helper_spec.rb
@@ -17,7 +17,10 @@ describe AwsHelper do
     it "should work without headers" do
       result = AwsHelper.build_s3_str_to_sign(:http_verb => "GET", :date => "Tue, 27 Mar 2007 19:44:46 +0000", 
         :resource => "/johnsmith/?acl")
-      result.should == "GET\n\n\nTue, 27 Mar 2007 19:44:46 +0000\n/johnsmith/?acl"
+        
+      expected_result = "GET\n\n\nTue, 27 Mar 2007 19:44:46 +0000\n/johnsmith/?acl"
+      
+      expect(result).to eq(expected_result)
     end
 
     it "should work with amazon headers" do
@@ -25,7 +28,9 @@ describe AwsHelper do
         :content_md5 => "4gJE4saaMU4BqNR0kLY+lw==", :content_type => "application/x-download",
         :resource => "/static.johnsmith.net/db-backup.dat.gz", 
         :amz_headers => ["x-amz-acl:public-read", "x-amz-meta-checksumalgorithm:crc32"])
-      result.should == "PUT\n4gJE4saaMU4BqNR0kLY+lw==\napplication/x-download\nTue, 27 Mar 2007 21:06:08 +0000\nx-amz-acl:public-read\nx-amz-meta-checksumalgorithm:crc32\n/static.johnsmith.net/db-backup.dat.gz"
+      
+        expected_result = "PUT\n4gJE4saaMU4BqNR0kLY+lw==\napplication/x-download\nTue, 27 Mar 2007 21:06:08 +0000\nx-amz-acl:public-read\nx-amz-meta-checksumalgorithm:crc32\n/static.johnsmith.net/db-backup.dat.gz"
+      expect(result).to eq(expected_result)
     end
 
   end
@@ -38,7 +43,12 @@ describe AwsHelper do
         :resource => "/johnsmith/photos/puppy.jpg"
       }
       key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-      AwsHelper.build_s3_rest_signature(key, options).should == "bWq2s1WEIj+Ydj0vQ697zp+IXMU="
+      
+      expected_result = "bWq2s1WEIj+Ydj0vQ697zp+IXMU="
+      
+      result = AwsHelper.build_s3_rest_signature(key, options)  
+      
+      expect(result).to eq(expected_result)
     end
 
   end
@@ -54,7 +64,9 @@ describe AwsHelper do
       secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
       access_key = "AKIAIOSFODNN7EXAMPLE"
       url = AwsHelper.build_s3_upload_url("/photos/puppy.jpg", access_key, secret_key, options)
-      url.should == "/photos/puppy.jpg?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Expires=1175139620&Signature=NpgCjnDzrM%2BWFzoENXmpNDUsSn8%3D"
+      
+      expected_result = "/photos/puppy.jpg?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Expires=1175139620&Signature=NpgCjnDzrM%2BWFzoENXmpNDUsSn8%3D"
+      expect(url).to eq(expected_result)
     end
 
   end
